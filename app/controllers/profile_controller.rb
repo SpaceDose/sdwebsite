076b2d722082ccd5788de3af40a5cda74047568c
SpaceDose <jan.ellermann@t-online.de>
2018-05-19 00:43:25 +0200

    Fixed bug that edit profile changes wrong users data

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,24 +2,24 @@ class ProfileController < ApplicationController
 protect_from_forgery
 
   def profile
-    $user = current_user
+
   end
 
   def edit_profile
-    $user = current_user
+
   end
 
   def save_nickname
     @new_nickname = params[:nickname]
-    $user.tsusername = @new_nickname
-    $user.save
+    current_user.tsusername = @new_nickname
+    current_user.save
     redirect_to profile_path
   end
 
   def save_uniqueid
     @new_unique_id = params[:uniqueid]
-    $user.uniqueid = @new_unique_id
-    $user.save
+    current_user.uniqueid = @new_unique_id
+    current_user.save
     redirect_to profile_path
   end
 
